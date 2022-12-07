23c96f13e2115f88271a4cf4523edfcca5f80c93
Félix Ortega Hortigüela <felix.ortega@cabify.com>
2022-12-07 10:31:48 +0100

    Day 7

---
+++ 2022/07/main2.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type treeNode struct {
+	parent   *treeNode
+	children map[string]*treeNode
+	size     int
+	path     string
+	t        string
+}
+
+func main() {
+	filePath := os.Args[1]
+	readFile, err := os.Open(filePath)
+	defer readFile.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	var tree, actual_node *treeNode
+	tree = nil
+	actual_node = nil
+	for fileScanner.Scan() {
+		buff := fileScanner.Text()
+
+		fmt.Printf("CLI: %s\n", buff)
+		if "$" == string(buff[0]) {
+			// Command mode
+			if "cd" == buff[2:4] {
+				// Change dir
+				new_path := buff[5:]
+				if new_path == ".." {
+					actual_node = actual_node.parent
+				} else if new_path == "/" {
+					// Root node
+					new_dir := treeNode{path: new_path, size: 0, parent: actual_node, children: make(map[string]*treeNode), t: "d"}
+					tree = &new_dir
+					actual_node = &new_dir
+				} else {
+					new_dir := treeNode{path: new_path, size: 0, parent: actual_node, children: make(map[string]*treeNode), t: "d"}
+					fmt.Printf("cd, new dir: %s\n", new_dir.path)
+					actual_node.children[new_path] = &new_dir
+					actual_node = &new_dir
+				}
+			}
+		} else {
+			// Inside of dir
+			if "dir" != buff[0:4] {
+				// File
+				strSize := strings.Split(buff, " ")[0]
+				path := strings.Split(buff, " ")[1]
+				intSize, _ := strconv.Atoi(strSize)
+				new_file := treeNode{path: path, size: intSize, parent: actual_node, children: nil, t: "f"}
+				actual_node.children[path] = &new_file
+			}
+
+		}
+	}
+	fmt.Println("------------------------")
+	tree.printTree(0)
+
+	// Let's walk the tree breadth first and accumulating sizes upwards
+	tree.calculateSizes()
+
+	fmt.Println("------------------------")
+	tree.printTree(0)
+
+	totalFSSize := tree.size
+	totalFreeSpace := 70000000 - totalFSSize
+	neededSpace := 30000000 - totalFreeSpace
+
+	fmt.Println("------------------------")
+	fmt.Printf("total: %d, free: %d, neeeded: %d\n", totalFSSize, totalFreeSpace, neededSpace)
+
+	dirsWithSizes := tree.extractDirs()
+
+	actual_size := 70000000
+	for i := 0; i < len(dirsWithSizes); i++ {
+		actual_dir := dirsWithSizes[i]
+		fmt.Printf("%s -> %d\n", actual_dir.path, actual_dir.size)
+		if actual_dir.size >= neededSpace && actual_dir.size < actual_size {
+			actual_size = actual_dir.size
+		}
+	}
+
+	fmt.Println(actual_size)
+
+}
+
+func (tree treeNode) printTree(indent int) {
+	padding := ""
+	for i := 0; i < indent; i++ {
+		padding += " "
+	}
+	if tree.t == "d" {
+		fmt.Printf("%s- %s (dir, accsize: %d)\n", padding, tree.path, tree.size)
+		for _, v := range tree.children {
+			v.printTree(indent + 2)
+		}
+	} else {
+		fmt.Printf("%s- %s (file, size=%d)\n", padding, tree.path, tree.size)
+	}
+}
+
+func (tree *treeNode) calculateSizes() int {
+	if tree.t == "d" {
+		tree.size = 0
+		for _, v := range tree.children {
+			tree.size += v.calculateSizes()
+		}
+	}
+	// fmt.Printf("path: %s, size: %d, type: %s\n", tree.path, tree.size, tree.t)
+	return tree.size
+}
+
+func (tree *treeNode) extractDirs() []*treeNode {
+	ret := []*treeNode{}
+	if tree.t == "d" {
+		ret = append(ret, tree)
+		for _, v := range tree.children {
+			ret = append(ret, v.extractDirs()...)
+		}
+	}
+	return ret
+}
