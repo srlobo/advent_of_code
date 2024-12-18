c4ee7d3ebe8155366ccebbf382d9a8435ce62ef6
Félix Ortega Hortigüela <fortegah@gmail.com>
2024-12-18 11:05:11 +0100

    Updated template with better dijkstra

--- 2024/template/solution1.go
+++ 2024/template/solution1.go
@@ -3,9 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
-	"strconv"
+	"regexp"
 )
 
 var empty = struct{}{}
@@ -49,6 +48,14 @@ type Board struct {
 	minY int
 	maxX int
 	maxY int
+}
+
+func makeBoardFromCoordArray(coord []Coord, maxX, maxY int) Board {
+	board := Board{elements: make(BoardElements), minX: 0, minY: 0, maxX: maxX, maxY: maxY}
+	for _, c := range coord {
+		board.elements[c] = '#'
+	}
+	return board
 }
 
 func (board *Board) drawMap() {
@@ -127,33 +134,53 @@ func abs(x int) int {
 	return x
 }
 
-func dijkstra(board Board, start, end Coord) int {
+func dijkstra(board Board, start, end Coord) (int, bool) {
 	getNeighbours := func(coord Coord) map[Coord]int {
 		ret := make(map[Coord]int)
 		var c Coord
 
 		// left
 		c = Coord{X: coord.X - 1, Y: coord.Y}
-		ret[c], _ = strconv.Atoi(string(board.elements[c]))
+		if c.X >= board.minX && c.X < board.maxX && c.Y >= board.minY && c.Y < board.maxY {
+			if _, ok := board.elements[c]; !ok {
+				ret[c] = 1
+			}
+		}
 
 		// right
 		c = Coord{X: coord.X + 1, Y: coord.Y}
-		ret[c], _ = strconv.Atoi(string(board.elements[c]))
+		if c.X >= board.minX && c.X < board.maxX && c.Y >= board.minY && c.Y < board.maxY {
+			if _, ok := board.elements[c]; !ok {
+				ret[c] = 1
+			}
+		}
 
 		// up
 		c = Coord{X: coord.X, Y: coord.Y - 1}
-		ret[c], _ = strconv.Atoi(string(board.elements[c]))
+		if c.X >= board.minX && c.X < board.maxX && c.Y >= board.minY && c.Y < board.maxY {
+			if _, ok := board.elements[c]; !ok {
+				ret[c] = 1
+			}
+		}
 
 		// down
 		c = Coord{X: coord.X, Y: coord.Y + 1}
-		ret[c], _ = strconv.Atoi(string(board.elements[c]))
+		if c.X >= board.minX && c.X < board.maxX && c.Y >= board.minY && c.Y < board.maxY {
+			if _, ok := board.elements[c]; !ok {
+				ret[c] = 1
+			}
+		}
 
 		return ret
 	}
 
 	getNextNode := func(unvisitedSet map[Coord]int) Coord {
-		min := math.MaxInt32
+		min := MaxInt
 		var ret Coord
+		for coord := range unvisitedSet { // Init the ret variable
+			ret = coord
+			break
+		}
 		for coord, v := range unvisitedSet {
 			if v < min {
 				min = v
@@ -167,7 +194,7 @@ func dijkstra(board Board, start, end Coord) int {
 	visitedSet := make(map[Coord]int)
 	for j := 0; j < board.maxY; j++ {
 		for i := 0; i < board.maxX; i++ {
-			unvisitedSet[Coord{X: i, Y: j}] = math.MaxInt32
+			unvisitedSet[Coord{X: i, Y: j}] = MaxInt
 		}
 	}
 	unvisitedSet[start] = 0
@@ -186,10 +213,16 @@ func dijkstra(board Board, start, end Coord) int {
 		delete(unvisitedSet, currentNode)
 		visitedSet[currentNode] = currentNodeDistance
 		if _, ok := visitedSet[end]; ok {
-			break
+			return visitedSet[end], true
+		}
+		if len(unvisitedSet) == 0 {
+			return visitedSet[currentNode], false
 		}
 		nextNode := getNextNode(unvisitedSet)
+		if unvisitedSet[nextNode] == MaxInt {
+			return visitedSet[currentNode], false
+		}
+
 		currentNode = nextNode
 	}
-	return visitedSet[end]
 }
